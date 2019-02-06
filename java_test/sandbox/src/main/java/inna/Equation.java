939925fc200d3e570ff5037d7884d43a958940fa
Inna <snowisland@list.ru>
2019-02-06 11:52:54 +0500

    If then else for Equation

--- java_test/sandbox/src/main/java/inna/Equation.java
+++ java_test/sandbox/src/main/java/inna/Equation.java
@@ -16,10 +16,22 @@ public class Equation {
 
     double d = b*b - 4*a*c;
 
-    if (d > 0) {
-      n = 2;
-    } else {
-      if (d == 0) {
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
+        n = 1;
+      }
+    }
+        else {
+
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;

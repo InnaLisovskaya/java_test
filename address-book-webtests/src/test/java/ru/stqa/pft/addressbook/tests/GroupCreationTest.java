6386302f8e742c634f49a1fbdc5558acce08a920
Inna <snowisland@list.ru>
2019-02-25 11:07:10 +0500

    Теперь считаем группы на правильной странице

--- address-book-webtests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ address-book-webtests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -7,8 +7,8 @@ import ru.stqa.pft.addressbook.models.GroupData;
 public class GroupCreationTest extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
-    int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", "test2", null));
     app.getNavigationHelper().gotoGroupPage();
     int after = app.getGroupHelper().getGroupCount();
--- address-book-webtests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ address-book-webtests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.models.GroupData;
 
@@ -12,11 +13,15 @@ public class GroupModificationTest  extends TestBase {
       app.getGroupHelper().createGroup(new GroupData("For deletion", "Test 2", "Test 3"));
     }
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().modifySelectedGroups();
     app.getGroupHelper().fillInGroupForm(new GroupData("Test 5", "Test 5", "Test 5"));
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().gotoGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+
+    Assert.assertEquals(after, before );
   }
 
 }

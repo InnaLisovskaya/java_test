00ff54a1caec46b94775e01ce8390ac63f9fc390
Inna <snowisland@list.ru>
2019-02-25 12:14:48 +0500

    Сравнение список как объектов, методы сравнения и преобразования в строку в классе GroupData

--- address-book-webtests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
+++ address-book-webtests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTest.java
@@ -20,5 +20,13 @@ public class GroupDeletionTest extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+
+    /*for (int i = 0; i < after.size(); i++) {
+      Assert.assertEquals(before.get(i), after.get(i));
+    } */
+
+    Assert.assertEquals(before, after);
   }
 }
